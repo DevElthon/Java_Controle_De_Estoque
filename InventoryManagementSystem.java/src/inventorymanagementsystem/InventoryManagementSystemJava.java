a12a0af10adfaefed7afe5369e1bba4eb9e1bdf0
Elthon Douglas Silva Bicudo <develthon@gmail.com>
2024-04-05 00:53:09 -0300

    criando pagina de login, necessario terminar integracao de banco

---
+++ InventoryManagementSystem.java/src/inventorymanagementsystem/InventoryManagementSystemJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventorymanagementsystem;
+
+/**
+ *
+ * @author eltho
+ */
+public class InventoryManagementSystemJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
