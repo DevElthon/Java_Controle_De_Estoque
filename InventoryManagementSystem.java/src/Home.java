79699789d6d93a7f019bbf31918644bf35dc6cd4
Elthon Douglas Silva Bicudo <develthon@gmail.com>
2024-04-10 21:56:55 -0300

    Pagina de cadastro e atualizacao de dados dos usuarios funcionando

--- InventoryManagementSystem.java/src/Home.java
+++ InventoryManagementSystem.java/src/Home.java
@@ -112,6 +112,7 @@ public class Home extends javax.swing.JFrame {
 
     private void btnUserActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUserActionPerformed
         // TODO add your handling code here:
+        new ManageUser().setVisible(true);
     }//GEN-LAST:event_btnUserActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
